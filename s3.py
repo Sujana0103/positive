90810b981424e92ace00b5ed6e2044d2d1f6985f
Gracekanagaraj <52075080+Gracekanagaraj@users.noreply.github.com>
2019-06-28 16:28:23 +0530

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,7 @@
+s=input()
+if s in ('a','e','i','o','u'):
+  print("Vowel")
+elif s in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z'):
+  print("Consonant")
+else:
+  print("invalid")
